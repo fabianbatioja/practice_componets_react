e7101e852394d114b9b080072fd5662532da4144
Fabian <fabian.batioja@openlms.net>
2024-09-17 20:09:19 -0500

    My first componente

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { MyFirstComponent } from './components/MyFirstComponent'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Fundametos de React</h1>
+      <MyFirstComponent />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,14 @@
+export const MyFirstComponent = () => {
+    return (
+        <>
+            <div>My First Component</div>
+            <h1>Temas de React</h1>
+            <ul>
+                <li>Componentes</li>
+                <li>Eventos</li>
+                <li>Estados Hook</li>
+                <li>Props</li>
+            </ul>
+        </>
+    )
+}
